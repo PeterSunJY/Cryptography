23e4f0b8b2de508a1b9c96fa18304917d8f8fa80
Jiayuan Sun <mrsunjiayuan@gmail.com>
2020-04-12 02:15:53 -0600

    Improving the code style

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 This program is used to encode and decode various kinds of cipher.
 This program runs in terminal. The python3 interpreter is needed.
 
-Version: 4/11/2020 from Pycharm
+Version: 4/12/2020 from Pycharm
 """
 
 import os
@@ -19,16 +19,16 @@ class ReadAndWrite:
         self.localPath = os.getcwd()
 
     @staticmethod
-    def FileRead():
+    def file_read():
         fp = open("input_text.txt", "r")
-        readList = fp.read()
+        read_list = fp.read()
         fp.close()
-        return readList
+        return read_list
 
     @staticmethod
-    def FileWrite(writeList):
+    def file_write(write_list):
         fp = open("output_text.txt", "w")
-        fp.write(writeList)
+        fp.write(write_list)
         fp.close()
         return ()
 
@@ -39,21 +39,21 @@ class UniversalInput:
     different kinds of input.
     """
 
-    def __init__(self, textType=None):
-        self.textType = textType
+    def __init__(self, text_type=None):
+        self.textType = text_type
 
     @staticmethod
-    def checkModeInput(actualInput, validInput):
+    def check_mode_input(actual_input, valid_input):
         """
         This function is used to check if user input invalid data.
         """
-        if actualInput not in validInput:
+        if actual_input not in valid_input:
             print("The input is not valid, please try again.\n")
             return False
         else:
             return True
 
-    def GeneralizedCaesarCodesInput(self, mode3):
+    def generalized_caesar_codes_input(self, mode3):
         text = None
         while True:
             try:
@@ -64,11 +64,11 @@ class UniversalInput:
         if mode3 == "1":
             text = input("Please input the " + self.textType + " :\n")
         elif mode3 == "2":
-            text = ReadAndWrite().FileRead()
-        inputList = [k, text]
-        return inputList
+            text = ReadAndWrite().file_read()
+        input_list = [k, text]
+        return input_list
 
-    def linearCodesInput(self, mode3):
+    def linear_codes_input(self, mode3):
         text = None
         while True:
             try:
@@ -85,9 +85,9 @@ class UniversalInput:
         if mode3 == "1":
             text = input("Please input the " + self.textType + " :\n")
         elif mode3 == "2":
-            text = ReadAndWrite().FileRead()
-        inputList = [a, k, text]
-        return inputList
+            text = ReadAndWrite().file_read()
+        input_list = [a, k, text]
+        return input_list
 
 
 class GeneralizedCaesarCodes:
@@ -96,7 +96,7 @@ class GeneralizedCaesarCodes:
     """
 
     def __init__(self, k, ciphertext, plaintext):
-        self.ciphertextInput = ciphertext
+        self.ciphertext_input = ciphertext
         self.plaintextInput = plaintext
         self.k = k
         self.plaintext = []
@@ -122,7 +122,7 @@ class GeneralizedCaesarCodes:
         return self.ciphertext
 
     def decode(self):
-        for i in self.ciphertextInput:
+        for i in self.ciphertext_input:
             if i == " ":
                 self.plaintext.append(i)
             elif i.isupper():
@@ -152,7 +152,7 @@ class LinearCodes:
     """
 
     def __init__(self, a, k, ciphertext, plaintext):
-        self.ciphertextInput = ciphertext
+        self.ciphertext_input = ciphertext
         self.plaintextInput = plaintext
         self.a = a
         self.k = k
@@ -180,7 +180,7 @@ class LinearCodes:
         return self.ciphertext
 
     def decode(self):
-        for i in self.ciphertextInput:
+        for i in self.ciphertext_input:
             if i == " ":
                 self.plaintext.append(i)
             elif i.isupper():
@@ -215,7 +215,7 @@ def mode():
         mode1 = input("Please input number to choose the type of cipher.\n\
             1. Generalized Caesar Codes\n\
             2. LinearCodes\n")
-        if UniversalInput().checkModeInput(mode1, ["1", "2"]):
+        if UniversalInput().check_mode_input(mode1, ["1", "2"]):
             break
         else:
             continue
@@ -223,7 +223,7 @@ def mode():
         mode2 = input("Please input number to choose encode or decode.\n\
             1. Encode\n\
             2. Decode\n")
-        if UniversalInput().checkModeInput(mode2, ["1", "2"]):
+        if UniversalInput().check_mode_input(mode2, ["1", "2"]):
             break
         else:
             continue
@@ -231,32 +231,32 @@ def mode():
         mode3 = input("Please input number to choose input method.\n\
             1. Manual input\n\
             2. Read file \"input_text.txt\" at local path\n")
-        if UniversalInput().checkModeInput(mode3, ["1", "2"]):
+        if UniversalInput().check_mode_input(mode3, ["1", "2"]):
             break
         else:
             continue
 
     if mode1 == "1":
         if mode2 == "1":
-            inputlist = UniversalInput("plaintext").GeneralizedCaesarCodesInput(mode3)
-            GCC = GeneralizedCaesarCodes(inputlist[0], 0, inputlist[1])
-            ReadAndWrite().FileWrite(''.join(GCC.encode()))
+            input_list = UniversalInput("plain text").generalized_caesar_codes_input(mode3)
+            gcc = GeneralizedCaesarCodes(input_list[0], 0, input_list[1])
+            ReadAndWrite().file_write(''.join(gcc.encode()))
 
         elif mode2 == "2":
-            inputlist = UniversalInput("ciphertext").GeneralizedCaesarCodesInput(mode3)
-            GCC = GeneralizedCaesarCodes(inputlist[0], inputlist[1], 0)
-            ReadAndWrite().FileWrite(''.join(GCC.decode()))
+            input_list = UniversalInput("cipher text").generalized_caesar_codes_input(mode3)
+            gcc = GeneralizedCaesarCodes(input_list[0], input_list[1], 0)
+            ReadAndWrite().file_write(''.join(gcc.decode()))
 
     elif mode1 == "2":
         if mode2 == "1":
-            inputlist = UniversalInput("plaintext").linearCodesInput(mode3)
-            LC = LinearCodes(inputlist[0], inputlist[1], 0, inputlist[2])
-            ReadAndWrite().FileWrite(''.join(LC.encode()))
+            input_list = UniversalInput("plain text").linear_codes_input(mode3)
+            lc = LinearCodes(input_list[0], input_list[1], 0, input_list[2])
+            ReadAndWrite().file_write(''.join(lc.encode()))
 
         elif mode2 == "2":
-            inputlist = UniversalInput("ciphertext").linearCodesInput(mode3)
-            LC = LinearCodes(inputlist[0], inputlist[1], inputlist[2], 0)
-            ReadAndWrite().FileWrite(''.join(LC.decode()))
+            input_list = UniversalInput("cipher text").linear_codes_input(mode3)
+            lc = LinearCodes(input_list[0], input_list[1], input_list[2], 0)
+            ReadAndWrite().file_write(''.join(lc.decode()))
 
 
 def main():
